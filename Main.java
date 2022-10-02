23c57edaa7e6663906fe7f44a32b38207427e140
Cansu Yoldaş <61535708+cansuyoldas@users.noreply.github.com>
2022-10-03 00:22:00 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,27 +1,12 @@
-package Calculator;
+package CreditManagement;
 
 public class Main {
 
 	public static void main(String[] args) {
-		//Week 2 / Video 30: Hesap makinası yapma
-		Transactions t = new Transactions(10,2);
-		t.result(t.doAddition());
-		t.result(t.doDivision());
-		t.result(t.doMultiplication());
-		t.result(t.doSubtraction());
-		
-		//or
-		
-		int additionResult = t.doAddition();
-		int divisionResult = t.doDivision();
-		int multiplicationResult = t.doMultiplication();
-		int subtractionResult = t.doSubtraction();
-		
-		System.out.println(" Addition Result is " + additionResult + System.lineSeparator() +
-						   " Division Result is " + divisionResult + System.lineSeparator() +
-						   " Multiplication Result is " + multiplicationResult + System.lineSeparator() +
-						   " Subtraction Result is " + subtractionResult);
-		
+		// Week 2 / Video 37 : Credit management
+		CreditUI ui = new CreditUI();
+		ui.creditCalculate(new TeacherCreditManager());
+
 	}
 
 }
---
+++ TeacherCreditManager.java
@@ -0,0 +1,5 @@
+package CreditManagement;
+
+public class TeacherCreditManager extends BaseCreditManager{
+
+}
